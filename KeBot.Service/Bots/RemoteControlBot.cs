4b9a497803db0e0475fc6cf47292ed6b6c166169
Ke Zhu <ke_zhu@msn.com>
2013-04-09 12:06:45 -0700

    remote bot

---
+++ KeBot.Service/Bots/RemoteControlBot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeBot.Service.Bots
+{
+    public class RemoteControlBot : IBot
+    {
+        public string Process(dynamic input)
+        {
+            string t = input.text;
+            if (t == "kebot pause")
+            {
+                KeBot.Run = false;
+                return "Kebot paused";
+            }
+            if (t == "kebot resume")
+            {
+                KeBot.Run = true;
+                return "Kebot resumed";
+            }
+            return null;
+        }
+    }
+}
